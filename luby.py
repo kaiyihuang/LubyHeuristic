f311b115c44f25fb9cf5e9a50f70de77e7236deb
kaiyihuang <iheartgrues@gmail.com>
2020-08-06 02:32:39 -0700

    Add files via upload

--- luby.py
+++ luby.py
@@ -15,17 +15,12 @@ import ast
 
 
 
-#CurrGraph = Graph.Read_Ncol("C:/dev/facebook/107.edges",directed=False)
+CurrGraph = Graph.Read_Ncol("C:/dev/facebook/107.edges",directed=False)
 lock_g = Lock()
 framey = pandas.read_csv("C:/dev/facebook_clean_data/new_sites_edges.csv",header=None)
-CurrGraph = Graph.TupleList(framey.itertuples(index=False),directed=False,weights=False)
-CurrGraph.simplify(multiple=True, loops=True, combine_edges=None)
+#CurrGraph = Graph.TupleList(framey.itertuples(index=False),directed=False,weights=False)
+#CurrGraph.simplify(multiple=True, loops=True, combine_edges=None)
 
-
-#CellZone = []
-
-
-#shm_a = shared_memory.SharedMemory(create=True, size=1073741824)
 def chunks(l, n):
     n = max(1, n)
     return (l[i:i+n] for i in range(0, len(l), n))
@@ -71,7 +66,6 @@ def lubys_func():
         normy_vs.append(v.index)
     for e in TopWorkSet.es:
         normy_es.append(e.index)
-    #Timer Starts here
     while len(TopWorkSet.vs) > 0:
         Z = []
         check = []
@@ -105,8 +99,6 @@ def lubys_func():
 
         outputSet = r1.intersection(r2,r3,r4)
 
-        #print(outputSet)
-
         X = outputSet #outputSet is a list of indices
 
         for Y in X:
@@ -115,13 +107,10 @@ def lubys_func():
             Z.append(Y)
         neigh = TopWorkSet.neighborhood(list(X),order=1)
 
-        #if(len(outputSet) == 0):
-            #return AnsSet
 
         for n in neigh:
             check += n
-        #check = check + Z
-        #check = set(check)
+
         try:
             TopWorkSet.delete_vertices(check)
         except:
@@ -137,10 +126,21 @@ def lubys_func():
 
 def lubys_wrap(limit):
     ans_set = set()
+    if limit < 0:
+        check = -1
+        while check < len(ans_set):
+            ans = lubys_func()
+            check = len(ans_set)
+            ans_set.add(frozenset(ans))
+            if check >= len(ans_set):
+                print (len(ans_set))
+                return ans_set
     while len(ans_set) < limit:
         ans = lubys_func()
-        print(verifyMIS(CurrGraph,ans))
         ans_set.add(frozenset(ans))
+    for ans in ans_set:
+        if not verifyMIS(CurrGraph,ans):
+            print("Invalid set detected")
     return ans_set
 
 def heur_lubys_func(heurstic):
@@ -270,9 +270,30 @@ def heurstic_wrapper(limit):
     max_heurstic_hist = 10
     heurstic_set = set()
     old_heurstic = set()
+    if limit < 0:
+        check = -1
+        while check < len(ans_set):
+            ans = heur_lubys_func(heurstic_set)
+            check = len(ans_set)
+            ans_set.add(frozenset(ans))
+            if check >= len(ans_set):
+                print (len(ans_set))
+                return ans_set
+            new_heur = set(random.sample(ans, int(len(ans)/4)) ) #Sample a fourth of the solution set into a set entity
+            
+
+            commons = set()
+            if len(old_heurstic) > 0:
+                commons = old_heurstic.intersection(ans)
+                commons = set(random.sample(commons, int(len(commons)/2)) )
+                ans = new_heur.union(commons)
+            else:
+                ans = new_heur
+
+            old_heurstic = heurstic_set
+            heurstic_set = ans
+
     while len(ans_set) < limit:
-        #if(len(heurstic_hist) > 0):
-            #heurstic_set = heurstic_hist[-1]
         ans = heur_lubys_func(heurstic_set)
         print(verifyMIS(CurrGraph,ans))
         ans_set.add(frozenset(ans) )
@@ -287,107 +308,128 @@ def heurstic_wrapper(limit):
             ans = new_heur.union(commons)
         else:
             ans = new_heur
-
-        #if not verifyMIS(CurrGraph,ans):
-            #continue
     
         old_heurstic = heurstic_set
         heurstic_set = ans
+    for ans in ans_set:
+        if not verifyMIS(CurrGraph,ans):
+            print("Invalid set detected")
     return ans_set
 
-def init(l):
-    global lock
-    lock = l
+
+def merge_dols(dol1, dol2, dol3, dol4):
+    keys = set(dol1).union(dol2,dol3,dol4)
+    no = []
+    return dict((k, dol1.get(k, no) + dol2.get(k, no) + dol3.get(k, no) + dol4.get(k, no)) for k in keys)
 
 def cellular_automata():
     global TopWorkSet 
-    #global CellZone
     checkList = set()
     returnList = []
     TopWorkSet = CurrGraph.copy()
-    #shm_a = shared_memory.SharedMemory(create=True, size=CurrGraph.vcount()*CurrGraph.vcount())
-    CellZone = shm_a.buf
-    #m = Manager()
-    #lock = m.Lock()
-    l = Lock()
-    pool = Pool(initializer=init, initargs=(l,))
-    with SharedMemoryManager() as smm:
-        while(TopWorkSet.maxdegree() > 0):
-            CellZone = ['x' * CurrGraph.vcount() * 40] * CurrGraph.vcount()
-            sync_list = []
-            n = TopWorkSet.vcount()
-            CellZoneShared = smm.ShareableList(CellZone)
-            #CellZoneShared = smm.SharedMemory(CurrGraph.vcount() * CurrGraph.vcount() * 5)
-            TopWorkSetShared = ray.put(TopWorkSet)
+    DeadList = dict()
 
-            for v in TopWorkSet.vs: #Broadcast the number to neighbors
-                sync_list.append(cell_work_1.remote(TopWorkSetShared,CellZoneShared,v.index))
+    
+    while(TopWorkSet.vcount() > 0):
+        n = TopWorkSet.vcount()
+        TopWorkShared = ray.put(TopWorkSet)
+        chunk = len(TopWorkSet.vs)
+        CellZone = dict()
+        '''
+        
+        c1 = cell_work_1.remote(TopWorkShared,0,int(chunk/4))
+        c2 = cell_work_1.remote(TopWorkShared,int(chunk/4),int(2*chunk/4))
+        c3 = cell_work_1.remote(TopWorkShared,int(2*chunk/4),int(3*chunk/4))
+        c4 = cell_work_1.remote(TopWorkShared,int(3*chunk/4),int(chunk))
 
-            for s in sync_list:
-                ray.get(s)
+        r1 = ray.get(c1)
+        r2 = ray.get(c2)
+        r3 = ray.get(c3)
+        r4 = ray.get(c4)
+        '''
+        for n in TopWorkSet.vs:
+            rand = random.random()
+            CellZone[n.index] = rand #Generate a random number and store it in your own register
 
-            sync_list.clear()
+        #CellZone = r1.update(r2.update(r3.update(r4)))
+        CellZoneShared = ray.put(CellZone)
 
-            for v in TopWorkSet.vs: #Each node fetches the top most 
-                sync_list.append(cell_work_2.remote(TopWorkSetShared,CellZoneShared,v.index))
+        c1 = cell_work_2.remote(TopWorkShared,CellZoneShared,0,int(chunk/4))
+        c2 = cell_work_2.remote(TopWorkShared,CellZoneShared,int(chunk/4),int(2*chunk/4))
+        c3 = cell_work_2.remote(TopWorkShared,CellZoneShared,int(2*chunk/4),int(3*chunk/4))
+        c4 = cell_work_2.remote(TopWorkShared,CellZoneShared,int(3*chunk/4),int(chunk))
 
-            for s in sync_list:
-                checkList.add(ray.get(s))
+        r1 = ray.get(c1)
+        r2 = ray.get(c2)
+        r3 = ray.get(c3)
+        r4 = ray.get(c4)
 
-            neighborz = TopWorkSet.neighborhood(list(checkList),order=1)
-            totes = []
-            returnList += list(checkList)
-            for neigh in neighborz:
-                totes += neigh
-            totes += list(checkList)
-            totes = set(totes)
-            TopWorkSet.delete_vertices(totes)
-            checkList = set()
+        checkList = r1+r2+r3+r4
+
+        if not verifyInd(CurrGraph,checkList):
+            pdb.set_trace()
+
+        neighborz = TopWorkSet.neighborhood(list(checkList),order=1) #Unfortunately, as the third step theoretically involves simultaneous mutation of an entity, it cannot be replicated here
+        totes = []
+        returnList += list(checkList)
+        for neigh in neighborz:
+            totes += neigh
+        totes += list(checkList)
+        totes = set(totes)
+        TopWorkSet.delete_vertices(totes)
+        checkList = set()
 
     return returnList
 
+@ray.remote
+def cell_work_1(workset,x,y): #Broadcast the number to neighbors
+    output = dict()
+    for n in workset.vs[x:y]:
+        rand = random.random()
+        output[n.index] = rand #Generate a random number and store it in your own register
+    return output
+
+
 
 @ray.remote
-def cell_work_1(workset,cellzone,v): #Broadcast the number to neighbors
-    anList = []
-    randNum = random.randint(1,workset.vcount()**4)
-    #shm_b = shared_memory.SharedMemory(name=cellzone, create=False)
-    for neigh in workset.neighbors(v):
-        lock.acquire()
-        cellzone[neigh] = cellzone[neigh].strip('x')
-        lock.release()
-        checkmate = (v, randNum)
-        try:
-            lock.acquire()
-            cellzone[neigh]  = cellzone[neigh] + ";" + str(checkmate)
-            lock.release()
-        except:
-            print(cellzone[neigh])
-
-@ray.remote
-def cell_work_2(workset,cellzone,v):
-    #shm_b = shared_memory.SharedMemory(name=cellzone, create=False)
-    if workset.degree(v) != 0:
-        sl = cellzone[v].split(";")
-        sl2 = []
-        for nuym in sl:
-            if nuym != '':
-                try:
-                    sl2.append(ast.literal_eval(nuym))
-                except:
-                    print(nuym)
-        sl2.sort(key = lambda x: x[1])
-        winrar = sl2[-1][0]
-    return winrar
+def cell_work_2(workset,cellzone,x,y):
+    output = []
+    for n in workset.vs[x:y]:
+        if(TopWorkSet.degree(n) == 0):
+            output.append(n.index)
+            continue
+        l = []
+        for neigh in workset.neighbors(n):
+            l.append((neigh, cellzone[neigh]))
+        l.sort(key = lambda x: x[1])
+        if l[0][1] > cellzone[n.index]: #You are the minimum random value generated
+            output.append(n.index)
+    return output
 
 def cellular_wrap(limit):
     ans_set = set()
     while len(ans_set) < limit:
         ans = cellular_automata()
         ans_set.add(frozenset(ans))
+        print(len(ans))
+    for ans in ans_set:
+        if not verifyMIS(CurrGraph,ans):
+            print("Invalid set detected")
     return ans_set
 
-    
+
+def verifyInd(graph, indSet):
+    setcull = set()
+    for v in indSet: #Verify if set is independent
+        if v in setcull:
+            print("Set is not independent")
+            return False
+        setcull.add(v)
+        neigh = graph.neighbors(v)
+        for n in neigh:
+            setcull.add(n)
+    return True
+
 def verifyMIS(graph, indSet):
     setcull = set()
     for v in indSet: #Verify if set is independent
@@ -397,8 +439,6 @@ def verifyMIS(graph, indSet):
         setcull.add(v)
         neigh = graph.neighbors(v)
         for n in neigh:
-            if(n in indSet):
-                print("Something's gone horribly wrong")
             setcull.add(n)
 
     if(len(graph.vs) == len(setcull)):
@@ -413,17 +453,19 @@ def ivs_wrapper(count):
 
 if __name__ == '__main__':
     Worset = CurrGraph.copy()
-    ray.init(num_cpus=8)
+    ray.init(num_cpus=4)
     print(CurrGraph.vcount())
     print( "Spinup: " + str(timeit.timeit('lubys_wrap(1)',setup='from __main__ import lubys_wrap',number=1)) ) # Always run this so Ray can initialize
+
+
     #temp = heurstic_wrapper(10)
-    print( "Heuristic: " + str(timeit.timeit('heurstic_wrapper(50)',setup='from __main__ import heurstic_wrapper',number=1)) )
-    print( "Non Heuristic: " + str(timeit.timeit('lubys_wrap(50)',setup='from __main__ import lubys_wrap',number=1)) )
+    #print( "Heuristic: " + str(timeit.timeit('heurstic_wrapper(-1)',setup='from __main__ import heurstic_wrapper',number=1)) )
+    #print( "Non Heuristic: " + str(timeit.timeit('lubys_wrap(-1)',setup='from __main__ import lubys_wrap',number=1)) )
 
 
     #print(timeit.timeit('ivs_wrapper(1) ',setup='from __main__ import ivs_wrapper',number=1))
     #print(timeit.timeit('naive_MIS()',setup='from __main__ import naive_MIS',number=1)/1)
     #print(timeit.timeit('naive_wrap(1)',setup='from __main__ import naive_wrap',number=1)/1)
-    #print(timeit.timeit('cellular_wrap(1)',setup='from __main__ import cellular_wrap',number=1))
+    print(timeit.timeit('cellular_wrap(1)',setup='from __main__ import cellular_wrap',number=1))
 
     #print(len(temp))
